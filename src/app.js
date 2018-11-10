86fb6683d91a40757fef7804cde2fb17029e966d
wang1212 <mrwang1212@126.com>
2018-11-10 17:09:53 +0800

    add content side navigation

--- src/app.js
+++ src/app.js
@@ -6,13 +6,15 @@ import controller from 'utils/controller.js';
 
 import { generate_category, auto_active_nav, add_header } from 'components/category.js';
 import { insert_line_num } from 'components/line-numbers.js';
+import { generate_navigation } from 'components/navigation.js';
 
 
 generate_category();
 
 controller([
-	() => document.body.scrollTop = 0,
+	() => (document.body.scrollTop = 0) || (document.documentElement.scrollTop = 0),
 	auto_active_nav,
 	add_header,
-	insert_line_num
+	insert_line_num,
+	generate_navigation
 ]);
---
+++ src/components/navigation.js
@@ -0,0 +1,53 @@
+/*! content navigation */
+
+// Generate navigation
+function generate_navigation() {
+
+	let h_datas = [],
+		nav_ele = document.createElement('div'),
+		html    = '';
+
+	['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].forEach(h => {
+		document.querySelectorAll(h).forEach(ele => {
+			h_datas.push({
+				type: h,
+				top : ele.getBoundingClientRect().top,
+				text: ele.textContent
+			});
+		});
+	});
+
+	nav_ele.classList.add('navigation');
+
+	h_datas.sort((a, b) => a.top - b.top).forEach(h => {
+		html += '<div class="d-block ' + h.type + '" s_top="' + h.top + '">' + h.text + '</div>';
+	});
+
+	nav_ele.innerHTML = html;
+
+	document.querySelector('.file-content').appendChild(nav_ele);
+
+	// scroll
+	window.onscroll = function() {
+		nav_ele.style.top = (document.body.scrollTop || document.documentElement.scrollTop) + 'px';
+	};
+
+	// click
+	setTimeout(() => {
+
+		Array.from(nav_ele.children).forEach(ele => {
+			ele.onclick = () => {
+				document.body.scrollTop            = +ele.getAttribute('s_top');
+				document.documentElement.scrollTop = +ele.getAttribute('s_top');
+			};
+		});
+
+	}, 100);
+}
+
+
+export {
+	generate_navigation
+};
+
+export default generate_navigation;
